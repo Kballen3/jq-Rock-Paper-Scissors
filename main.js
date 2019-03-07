9fe73dc1eea3718a59745a372ff133a39de0bcff
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-07 07:28:11 -0700

    commit try

---
+++ main.js
@@ -0,0 +1,105 @@
+function startGame(val) {
+    var userChoice = ["Rock", "Paper", "Scissors"]
+    switch (val) {
+        case 1:
+            userChoice[0]
+            break;
+        case 2:
+            userChoice[1]
+            break;
+        case 3:
+            userChoice[2]
+            break;
+    }
+    return userChoice; 
+}
+
+console.log(startGame(1))
+
+debugger
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready( function() {
+//     $('.btn').on('click', function() {
+//         var id = this.id;
+//         var userChoice = " ";
+//         switch(this.id) { 
+//             case 'rock':
+//             userChoice = 'rock'
+//             break;
+            
+//             case 'paper':
+//             userChoice = 'paper'
+//             break;
+            
+//             case 'scissors':
+//             userChoice = 'scisssors'
+//             break;
+            
+//         }
+//         return userChoice;
+//     })
+// debugger
+//     console.log()
+    
+
+    
+    // var compChoice
+    // function rockChoice() {
+    //     alert ("function works")
+    //     if ("rock") {
+            
+            
+    //     }
+    // }
+    
+    // debugger
+    //     var id =  this.id;
+    //     switch(this.id) {
+    //         case "rock":
+    //         whoWin()
+    //         break;
+
+    //         case "paper":
+    //         whoWin()
+    //         break;
+
+    //         case "scissors":
+    //         whoWin()
+    //         break;
+
+    //     }
+    // }
+    // debugger
+    // function whoWin() {
+    //     compChoice().id
+    // }
+
+
+   
+
+    // function () {
+
+    // }
+    //computer choice
+    //case 
+    //user choice
+    //case argument
+    //boolean case argument
+    
+    // pick winner
+    //keep track of wins and losses
+    
+
+    
+})
