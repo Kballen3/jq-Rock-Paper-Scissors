8867a117046319b700008616fb141e0faaafc6d5
Kaleb Allen <7kaleb29allen@gmail.com>
2019-06-03 11:09:23 -0600

    made popup for test

--- main.js
+++ main.js
@@ -1,105 +1,20 @@
-function startGame(val) {
-    var userChoice = ["Rock", "Paper", "Scissors"]
-    switch (val) {
-        case 1:
-            userChoice[0]
-            break;
-        case 2:
-            userChoice[1]
-            break;
-        case 3:
-            userChoice[2]
-            break;
-    }
-    return userChoice; 
+var modal = document.getElementById("modal")
+
+var button = document.getElementsByClassName("button")
+
+var span = document.getElementsByClassName("close") [0]
+
+button.onclick = function() {
+    modal.style.display = "block"
+    
 }
 
-console.log(startGame(1))
+span.onclick = function() {
+    modal.style.display = "none"
+}
 
-debugger
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready( function() {
-//     $('.btn').on('click', function() {
-//         var id = this.id;
-//         var userChoice = " ";
-//         switch(this.id) { 
-//             case 'rock':
-//             userChoice = 'rock'
-//             break;
-            
-//             case 'paper':
-//             userChoice = 'paper'
-//             break;
-            
-//             case 'scissors':
-//             userChoice = 'scisssors'
-//             break;
-            
-//         }
-//         return userChoice;
-//     })
-// debugger
-//     console.log()
-    
-
-    
-    // var compChoice
-    // function rockChoice() {
-    //     alert ("function works")
-    //     if ("rock") {
-            
-            
-    //     }
-    // }
-    
-    // debugger
-    //     var id =  this.id;
-    //     switch(this.id) {
-    //         case "rock":
-    //         whoWin()
-    //         break;
-
-    //         case "paper":
-    //         whoWin()
-    //         break;
-
-    //         case "scissors":
-    //         whoWin()
-    //         break;
-
-    //     }
-    // }
-    // debugger
-    // function whoWin() {
-    //     compChoice().id
-    // }
-
-
-   
-
-    // function () {
-
-    // }
-    //computer choice
-    //case 
-    //user choice
-    //case argument
-    //boolean case argument
-    
-    // pick winner
-    //keep track of wins and losses
-    
-
-    
-})
+window.onclick = function() {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
